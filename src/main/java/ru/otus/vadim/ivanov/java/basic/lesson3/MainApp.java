74bdcdf6edb5ac0f8fabc2745cf93de78372719b
vadim.s.ivanov <vds.ivanov@gmail.com>
2023-07-13 15:36:47 +0300

    greetings done

--- src/main/java/ru/otus/vadim/ivanov/java/basic/lesson3/MainApp.java
+++ src/main/java/ru/otus/vadim/ivanov/java/basic/lesson3/MainApp.java
@@ -4,5 +4,10 @@ public class MainApp {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        greetings();
+    }
+
+    public static void greetings() {
+        System.out.println("Hello\nWorld\nfrom\nJava");
     }
 }
