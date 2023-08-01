3d305a67e769a661ab9fb84865f31b2340934c15
vadim.s.ivanov <vds.ivanov@gmail.com>
2023-08-01 07:46:47 +0300

    fix i1,i2

--- src/main/java/ru/otus/vadim/ivanov/java/basic/lesson7/MainApp7.java
+++ src/main/java/ru/otus/vadim/ivanov/java/basic/lesson7/MainApp7.java
@@ -28,16 +28,17 @@ public class MainApp7 {
         System.out.println(sumOfSecondArrRaw(arr4));
 
         int[][] arr5 = {{1,3,34},{4,5,89}};
+
         System.out.println(sumOfSecondArrRaw(arr5));
 
     }
 
     public static int sumOfPositiveElements(int[][] arr) {
         int sum = 0;
-        for(int i1=0; i1 < arr.length;i1++){
-            for(int i2=0; i2 <arr[i1].length;i2++){
-                if(arr[i1][i2] > 0) {
-                   sum +=  arr[i1][i2];
+        for(int i=0; i < arr.length;i++){
+            for(int j=0; j <arr[i].length;j++){
+                if(arr[i][j] > 0) {
+                   sum +=  arr[i][j];
                 }
             }
         }
@@ -45,8 +46,8 @@ public class MainApp7 {
     }
 
     public static void drawBox(int size) {
-        for(int i1 = 0; i1 < size; i1++){
-            for(int i2 = 0; i2 < size; i2++){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
                 System.out.print("* ");
             }
             System.out.print("\n");
@@ -55,10 +56,10 @@ public class MainApp7 {
 
     public static void setNullOnDiagonal(int[][] arr) {
         int[] offset = {0,0};
-        for(int i1 = 0; i1 < arr.length; i1++) {
-            for(int i2 = 0; i2 < arr[i1].length; i2++) {
-                if( (offset[0] == i1) && (offset[1] == i2)) {
-                    arr[i1][i2] = 0;
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                if( (offset[0] == i) && (offset[1] == j)) {
+                    arr[i][j] = 0;
                     offset[0]++;
                     offset[1]++;
                 }
@@ -68,10 +69,10 @@ public class MainApp7 {
 
     public static int findMax(int[][] array) {
         int result = Integer.MIN_VALUE;
-        for(int i1 = 0; i1 < array.length;i1++) {
-            for(int i2 = 0; i2 < array[i1].length;i2++){
-               if( array[i1][i2] > result) {
-                   result = array[i1][i2];
+        for(int i = 0; i < array.length;i++) {
+            for(int j = 0; j < array[i].length;j++){
+               if( array[i][j] > result) {
+                   result = array[i][j];
                }
             }
         }
