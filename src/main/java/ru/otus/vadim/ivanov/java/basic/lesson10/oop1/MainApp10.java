97a596971d5f5cb28c20b54860edca90e96c225d
vadim.s.ivanov <vds.ivanov@gmail.com>
2023-08-05 16:08:44 +0300

    add hw10.oop1 MainApp

---
+++ src/main/java/ru/otus/vadim/ivanov/java/basic/lesson10/oop1/MainApp10.java
@@ -0,0 +1,7 @@
+package ru.otus.vadim.ivanov.java.basic.lesson10.oop1;
+
+public class MainApp10 {
+    public static void main(String[] args) {
+        System.out.println("----hw10.oop1----");
+    }
+}
